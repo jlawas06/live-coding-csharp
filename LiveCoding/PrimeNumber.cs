690a491d429bdfc165a9fad3f39212f1839dd17c
jlawas06 <jlawas06@gmail.com>
2024-08-16 22:10:25 +0800

    feat: prime number

---
+++ LiveCoding/PrimeNumber.cs
@@ -0,0 +1,20 @@
+﻿namespace LiveCoding;
+internal static class PrimeNumber
+{
+    public static bool IsPrime(int number)
+    {
+        if (number < 2) return false;
+
+        if (number == 2) return true;
+
+        if (number % 2 == 0) return false;
+
+        
+        for (int i = 3; i <= Math.Sqrt(number); i += 2)
+        {
+            if (number % i == 0) return false;
+        }
+
+        return true;
+    }
+}
