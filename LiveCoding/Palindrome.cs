a986b53379fe7a901c8555793de490dce11f1ca5
jlawas06 <jlawas06@gmail.com>
2024-08-16 22:10:09 +0800

    feat: palindrome

---
+++ LiveCoding/Palindrome.cs
@@ -0,0 +1,10 @@
+﻿namespace LiveCoding;
+internal static class Palindrome
+{
+    public static bool IsPalindrome(string str)
+    {
+        var reversedString = new string(str.Reverse().ToArray());
+
+        return str.Equals(reversedString);
+    }
+}
